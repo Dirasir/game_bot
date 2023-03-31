fc5dfa116901a50427d8ca82abd53222538a2272
Dirasir <baicyrovdaniil2007@gmail.om>
2023-03-31 12:57:20 +0300

    -рабочий вход и регистрация и заготовка меню

--- main_bot.py
+++ main_bot.py
@@ -43,7 +43,44 @@ def message_reply(message):
         if message.chat.id in id_login.keys():
             # тут все команды после входа
             if message.text == "Играть":
-                bot.send_message(message.chat.id, "Выберите игру из выпадающего списка")
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                item1 = types.KeyboardButton("Города")
+                item2 = types.KeyboardButton("Дота 3")
+                item3 = types.KeyboardButton("Геншин импакт")
+                item4 = types.KeyboardButton("Шелли и Кольт")
+                item5 = types.KeyboardButton("Назад")
+                markup.add(item1)
+                markup.add(item2)
+                markup.add(item3)
+                markup.add(item4)
+                markup.add(item5)
+                bot.send_message(message.chat.id, "Выберите игру из выпадающего списка",reply_markup=markup)
+            if message.text == "Профиль":
+                con = sqlite3.connect("progress_bd.db")
+                cur = con.cursor()
+                result = cur.execute(f"""SELECT * FROM progress WHERE login = '{id_login[message.chat.id]}'""").fetchone()
+                stroka = f"Имя: {result[1]}"
+                con.close()
+                bot.send_message(message.chat.id, stroka)
+            if message.text == "Выход":
+                id_login.pop(message.chat.id)
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                item1 = types.KeyboardButton("Войти")
+                item2 = types.KeyboardButton("Регистрация")
+                markup.add(item1)
+                markup.add(item2)
+                bot.send_message(message.chat.id, "Вы успешно вышли из аккаунта",reply_markup=markup)
+            if message.text == "Назад":
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                item1 = types.KeyboardButton("Играть")
+                item2 = types.KeyboardButton("Профиль")
+                item3 = types.KeyboardButton("Выход")
+                markup.add(item1)
+                markup.add(item2)
+                markup.add(item3)
+                bot.send_message(message.chat.id, "Назад", reply_markup=markup)
+
+
         else:
             bot.send_message(message.chat.id, "Вы ещё не зашли")
 
@@ -78,12 +115,12 @@ def registration(message):
     if len(message_text) == 3 and message_text[1] == message_text[2]:
         con = sqlite3.connect("progress_bd.db")
         cur = con.cursor()
-        if cur.execute(f"""SELECT * FROM progress WHERE login = '{message_text[0]}'""").fetchall() == None:
+        if cur.execute(f"""SELECT * FROM progress WHERE login = '{message_text[0]}'""").fetchone() == None:
             cur.execute(f"""INSERT INTO progress(login,password) VALUES ('{message_text[0]}', '{message_text[1]}')""")
             con.commit()
             con.close()
             bot.send_message(message.chat.id,
-                             f"Вы успешно зарегестрировались ваш \nЛогин: {message_text[0]} \nПароль: {message_text[1]}")
+                             f"Вы успешно зарегестрировались, ваш \nЛогин: {message_text[0]} \nПароль: {message_text[1]}")
         else:
             bot.send_message(message.chat.id, "Такой логин уже занят")
     else:
